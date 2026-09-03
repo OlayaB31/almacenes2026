a6d9fb1310357b1aac3738ba898da4ee48a1fc28
OlayaB31 <olayabetancur31@gmail.com>
2026-09-03 15:13:59 -0500

    Create types.ts

---
+++ frontend/src/types.ts
@@ -0,0 +1,22 @@
+export type Store = {
+  id?: number
+  codigo_wsiga: string
+  nit: string
+  nombre_almacen: string
+  direccion: string
+  centro_comercial: string
+  telefono_fijo: string
+  celular: string
+  whatsapp: string
+  correo_electronico: string
+  pagina_web: string
+  facebook: string
+  instagram: string
+  tiktok: string
+  created_at?: string
+  updated_at?: string
+}
+
+export type User = {
+  username: string
+}
